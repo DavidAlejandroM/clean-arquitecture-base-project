e919ebf744eb86497cde3669f1edaafbe05672d3
David Alejandro Marin Alzate <jandro240@gmail.com>
2022-01-03 22:22:54 -0500

    arquitectura base

---
+++ application/src/main/java/co/com/alejandrom/holamundo/HolamundoApplication.java
@@ -0,0 +1,15 @@
+package co.com.alejandrom.holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class HolamundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolamundoApplication.class, args);
+	}
+
+}
---
+++ application/src/test/java/co/com/alejandrom/holamundo/HolamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.alejandrom.holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
